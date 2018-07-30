ca24192540a017e3d48316635139e4b9b7c22cb1
Nasser Sanou <nassersanou23@gmail.com>
2018-07-30 05:32:24 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
- numbers = [1,2,3]
- numbers.each { |number| number ** 2
-  }
+# numbers = [1,2,3]
+# numbers.each { |number| number ** 2
+#   }
 end
 
 def square_array(new_numbers )
