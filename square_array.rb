ab24760b49239e1b5b10f0f41ac6a71b362fd305
Nasser Sanou <nassersanou23@gmail.com>
2018-07-30 05:32:29 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,12 @@ def square_array(array)
 # numbers = [1,2,3]
 # numbers.each { |number| number ** 2
 #   }
+ array = [1,2,3]
+    array.each do |i|puts
+      i ** 2
+    end
 end
 
-def square_array(new_numbers )
+# def square_array(new_numbers )
   
-end 
+# end 
