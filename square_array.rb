f054600ea7c53bcde5b1913f83958124b9188c37
Nasser Sanou <nassersanou23@gmail.com>
2018-07-30 06:01:55 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -4,19 +4,9 @@ def square_array(array)
   arr
 end
  array = [1,2,3]
-    array.each do |i|puts
-      i ** 2
-    end
-end
 
-def square_array(array)
-  arr = []
-  array.each { |i| arr << i ** 2 }
-  arr
-end
-
-my_arr = [1, 2]
 p square_array(my_arr)
+
 # def square_array(new_numbers )
   
 # end 
